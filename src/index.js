cd249515bf75df277b07130692b3dfba942536a5
Eduard Fedosevich <fedosewiched@gmail.com>
2019-03-02 03:55:58 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;  
+  for (let i = 0; i < preferences.length; i++) {
+      if (preferences[preferences[preferences[i]-1]-1]-1 === i){
+          count++
+      }
+  }  return Math.floor(count/3); 
 };
